f80343efe4825c2c19d102529e51ca9283a26cd3
Emmanuel Oduntan <oduntane@icloud.com>
2025-10-16 08:52:30 +0100

    refactor: remove unused variable

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var profileRouter = require('./routes/profile');
 
 var app = express();
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
